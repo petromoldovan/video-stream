e657344a760f09d95e4886547704111f9b22acda
petromoldovan <petro.moldovan@dcso.de>
2019-01-21 09:11:42 +0100

    ranged request

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const http = require("http")
 const https = require("https")
 const {authRoute} = require('./helpers/auth_utils')
 const config = require('config')
+const { promisify } = require('util')
 
 const app = express()
 
@@ -13,52 +14,61 @@ app.get("/ping", (req, res, next) => {
   return res.status(200).send({message: "ok"})
 })
 
+app.get("/copyfile-long-version", (req, res) => {
+  const input = `${__dirname}/../public/test.mp4`
+  const output = `${__dirname}/../public/copy.mp4`
+  const readStream = fs.createReadStream(input)
+  const writeStream = fs.createWriteStream(output)
+
+  readStream.on('data', (chunk) => {
+    const isReadyToWriteMore = writeStream.write(chunk)
+    if (!isReadyToWriteMore) {
+      console.log("backpreasure. Stoping read stream")
+      readStream.pause()
+    }
+  })
+
+  writeStream.on('drain', () => {
+    console.log("drained")
+    readStream.resume()
+  })
+
+  readStream.on('end', () => {
+    console.log("file copied")
+    writeStream.end()
+  })
+})
+
+app.get("/copyfile-short-version", (req, res) => {
+  const input = `${__dirname}/../public/test.mp4`
+  const output = `${__dirname}/../public/copy.mp4`
+  const readStream = fs.createReadStream(input)
+  const writeStream = fs.createWriteStream(output)
+
+  readStream.pipe(writeStream)
+})
+
 app.get("/proxy", authRoute, (req, res) => {
   return res.status(200).send({message: "ok"})
 })
 
-app.get("/video", (req, res, next) => {
-  console.log("requesting video")
-  const path = "public/test.mp4"
+app.get("/video", async (req, res, next) => {
+  const path = `${__dirname}/../public/test.mp4`
   const stats = fs.statSync(path)
   const range = req.headers.range
-  console.log("req", req.headers.range)
-  console.log("size", stats.size)
-
   const fileSize = stats.size
+  const parts = range.replace(/bytes=/, "").split("-")
+  const start = parseInt(parts[0], 10)
+  const end = parts[1] ? parseInt(parts[1], 10) : fileSize - 1
 
-  if (range) {
-    const parts = range.replace(/bytes=/, "").split("-")
-    const start = parseInt(parts[0], 10)
-    /*const end = parts[1]
-      ? parseInt(parts[1], 10)
-      : fileSize-1*/
-
-    let chunk =  fileSize / 5
-    chunk = parseInt(parts[0], 10)
-    const end = start + chunk
-
-    //const chunksize = (end-start)+1
-    const file = fs.createReadStream(path, {start, end})
-
-
-
-    const head = {
-      'Content-Range': `bytes ${start}-${end}/${fileSize}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunk,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(206, head);
-    file.pipe(res);
-  } else {
-    const head = {
-      'Content-Length': fileSize,
-      'Content-Type': 'video/mp4',
-    }
-    res.writeHead(200, head)
-    fs.createReadStream(path).pipe(res)
+  const head = {
+    'Content-Range': `bytes ${start}-${end}/${fileSize}`,
+    'Accept-Ranges': 'bytes',
+    'Content-Length': (end-start) + 1,
+    'Content-Type': 'video/mp4',
   }
+  res.writeHead(206, head)
+  fs.createReadStream(path, {start, end}).pipe(res)
 })
 
 const createServer = (app) => {
